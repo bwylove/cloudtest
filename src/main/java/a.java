d3f54baf8de4aa7c651b34373431f03cadbfc972
bwylove <bwylove@126.com>
2018-03-12 17:54:33 +0800

    second

---
+++ src/main/java/a.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: a
+ * Author:   92406
+ * Date:     2018/3/12 17:53
+ * Description: a
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名          修改时间           版本号             描述
+ */
+
+
+public class a {
+}
