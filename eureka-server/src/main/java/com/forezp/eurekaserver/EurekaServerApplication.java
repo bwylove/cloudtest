e0b2851e6a090076b31b1955d74bcbb1046bc6f6
bwylove <bwylove@126.com>
2018-03-13 14:18:22 +0800

    server and client

--- eureka-server/src/main/java/com/forezp/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/forezp/eurekaserver/EurekaServerApplication.java
@@ -1,16 +1,14 @@
-package com.forezp.configserver;
+package com.forezp.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-@EnableConfigServer
-public class ConfigServerApplication {
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 }
